aef617a73fe0d3da80972a311345f01da1d09af8
takafumifujita <t.fujita@tudelft.nl>
2016-10-26 11:50:11 +0000

    takafumi_2

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 print('hello world')
 
 import numpy as np
+pi=np.pi
 
 def circumference(radius):
-    return 2*np.pi*radius #hello there!
+    return 2*pi*radius #hello there!
 
 def surface(radius):
-    return np.pi*radius**2 #sdadasdsad
+    return pi*radius**2 #sdadasdsad
