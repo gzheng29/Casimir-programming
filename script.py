06927d8a31967e3fd1c842dd5350a79bfa496671
takafumifujita <t.fujita@tudelft.nl>
2016-10-26 12:14:21 +0000

    TF_cleaned_script

--- script.py
+++ script.py
@@ -1,10 +1,6 @@
 print('fo shizzle ma nizzle')
 
-<<<<<<< HEAD
 from test import *
 
 print(circumference(1))
 print(surface(1))
-=======
-print('much better now')
->>>>>>> 27d0ad3ca9f185a35e7fb445dae90523a4f6051c
--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 print('hello world')
 
 import numpy as np
-<<<<<<< HEAD
-
-##GZ
-##GZ
-=======
-#TF
->>>>>>> 2e6d2c858aabaf911379d506637cc1d98bfadac9
 
 def circumference(radius):
     return 2*np.pi*radius 
