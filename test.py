225d29160e9b129b2429acbb2fa2ef69d8111d77
gzheng29 <gzheng29@gmail.com>
2016-10-26 09:37:20 +0000

    my fourth commit

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ print('hello world')
 
 import numpy as np
 
-def circumference(radius)
+def circumference(radius):
     return 2*np.pi*radius
 
-def surface(radius)
+def surface(radius):
     return np.pi*radius**2
