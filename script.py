4c7754c05e43538d256d923a4e85fd7de3c19179
gzheng29 <gzheng29@gmail.com>
2016-10-26 11:30:05 +0000

    collaborative commit

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
 print('fo shizzle ma nizzle')
+
+from test import *
+
+print(circumference(1))
+print(surface(1))
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ print('hello world')
 import numpy as np
 
 def circumference(radius):
-    return 2*np.pi*radius
+    return 2*np.pi*radius #hello there!
 
 def surface(radius):
     return np.pi*radius**2
