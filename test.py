1dfa09b428ca7187b4d7ecc5d2b7e242e6ece2e2
gzheng29 <gzheng29@gmail.com>
2016-10-26 11:53:54 +0000

    guoji

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 print('hello world')
 
 import numpy as np
-pi=np.pi
+
+##
+##
+##
 
 def circumference(radius):
-    return 2*pi*radius #hello there!
+    return 2*np.pi*radius #hello there!
 
 def surface(radius):
-    return pi*radius**2 #sdadasdsad
+    return np.pi*radius**2 #sdadasdsad
